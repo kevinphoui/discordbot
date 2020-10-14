1f36892f1f840bafd4e9ca0279786176598dd49a
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2020-10-13 23:39:07 -0500

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix='!';
+
+client.once('ready', () => {
+    console.log('kevin bot is online!');
+});
+
+client.on('message', message =>{
+    //ignore if no prefix
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'twitch'){
+        message.channel.send('https://www.twitch.tv/catblast');
+    } else if (command == 'help'){
+        message.channel.send(user);
+    }
+});
+
+
+client.login(process.env.token);
