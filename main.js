83b98717c7ac7e9c38511e708f43a30754603e62
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2020-10-14 01:37:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+//call bot command
 const prefix='!';
 
+//grab the "first" mentioned user from the message
+// this will return a 'user' object, just like 'message.author'
+const taggedUser = message.mentions.users.first();
 client.once('ready', () => {
     console.log('kevin bot is online!');
 });
@@ -19,19 +23,32 @@ client.on('message', message =>{
         message.channel.send('pong!');
     } else if (command == 'twitch'){
         message.channel.send('https://www.twitch.tv/catblast');
-    } else if (command == 'help'){
-        message.channel.send('idk what to help u with idiot');
-    } else if (command == 'fuck-you'){
-        message.channel.send('no fuck you!');
+    } else if (command == 'fuck.you'){
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag someone first!');
+        }
+        var randomNum = Math.random();
+        if (randomNum > 0.8){
+        message.channel.send(`Hey ${taggedUser}, fuck you!`);
+        }
+        else {
+            message.channel.send(`You know what? fuck you instead ${taggedUser}!`);
+        }
     } else if (command == 'kiss'){
         if (!message.mentions.users.size) {
             return message.reply('you need to tag someone first!');
         }
-        //grab the "first" mentioned user from the message
-        // this will retrun a 'user' object, just like 'message.author'
-        const taggedUser = message.mentions.users.first();
         message.channel.send(`<:twodudes1:765817861175902208>${taggedUser}<:twodudes2:765817874652594186>`);
-        
+    } else if (command == 'help'){
+        message.channel.send('!commands for commands');
+    } else if (command == 'commands'){
+        message.channel.send('!ping for pong.\n' +
+        '!twitch for cool streamer.\n' +
+        '!fuck.you to fuck someone off.\n' +
+        '!kiss to kiss someone.');
+    }
+    else{
+        message.channel.send('???');
     }
 });
 
