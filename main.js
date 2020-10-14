77448a2beb2abc3fee68d1b93fcf28e4f6902877
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2020-10-14 00:34:17 -0500

    new commands

--- main.js
+++ main.js
@@ -21,8 +21,13 @@ client.on('message', message =>{
         message.channel.send('https://www.twitch.tv/catblast');
     } else if (command == 'help'){
         message.channel.send('idk what to help u with idiot');
-    } else if (command == 'fuck you'){
+    } else if (command == 'fuck-you'){
         message.channel.send('no fuck you!');
+    } else if (command == 'kiss'){
+        //grab the "first" mentioned user from the message
+        // this will retrun a 'user' object, just like 'message.author'
+        const taggedUser = message.mentions.users.first();
+        message.channel.send(':twodudes1: ${taggedUser.username} :twodudes2:');
     }
 });
 
