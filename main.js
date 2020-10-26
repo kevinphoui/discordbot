89e34b145e7950fd9d6d9b3abf5da7c0bc838025
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2020-10-26 01:07:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
 //call bot command
-const prefix='!';
+const prefix="!";
 
 
-client.once('ready', () => {
-    console.log('kevin bot is online!');
+client.once("ready", () => {
+    console.log("kevin bot is online!");
 });
 
-client.on('message', message =>{
+client.on("message", message =>{
 
     /* to be fixed
-    if(message == 'pog'){
-        message.channel.send('poggers');
+    if(message == "pog"){
+        message.channel.send("poggers");
     } */
     //ignore if no prefix
     if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -23,69 +23,72 @@ client.on('message', message =>{
     const command = args.shift().toLowerCase();
 
     //grab the "first" mentioned user from the message
-    // this will return a 'user' object, just like 'message.author'
+    // this will return a "user" object, just like "message.author"
     const taggedUser = message.mentions.users.first();
 
     //this command may be redundant
     //help
-    if (command == 'help'){
-        message.channel.send('!commands for commands');
+    if (command == "help"){
+        message.channel.send("!commands for commands");
     } 
 
     //commands
-    else if (command == 'commands'){
-        message.channel.send('!info to learn about this bot\n' +
-        '!update for the latest updates\n' + 
-        '!hi to say hi\n' +
-        '!kiss to kiss someone\n' +
-        '!warn to send someone a useless warning\n');
+    else if (command == "commands"){
+        message.channel.send("!info to learn more about this bot\n" +
+        "!update for the latest updates\n" + 
+        "!hi to say hi\n" +
+        "!kiss to kiss someone\n" +
+        "!warn to send someone a useless warning\n");
     }
 
     //info
-    else if (command == 'info'){
-        message.channel.send('This bot is a work in progress!\n' +
-        "Made in 10/13/2020\n" +
+    else if (command == "info"){
+        message.channel.send("This is a relatively new bot!\n"+
+        "Currently a work in progress!\n" +
         "Made by Kevin Phouisangiem\n" +
+        "Created in 10/13/2020\n" +
+        "Made with node.js and JavaScript" +
         "Credit to CodeLyon for the online tutorials\n" +
         "Credit to Heroku for hosting this bot\n" +
-        "Check out our GitHub: https://github.com/kevinpho970/discordbot\n");
+        "This bot's GitHub: https://github.com/kevinpho970/discordbot\n");
     } 
     
-    else if (command == 'update'){
-        message.channel.send('10/15/20\n' +
-        'Removed !f u command\n' +
-        'Changed !ping pong command to hi hello\n' +
-        "Updated !commands\n" +
-        'Created !info command\n' +
-        'Created !update command\n' +
-        "Added todo comments\n" +
-        'Commented "pog" command out\n'
+    //update
+    else if (command == "update"){
+        message.channel.send("```Latest Update: 10/17/20\n" +
+        "Modified warning message\n" +
+        " ```"
         );
     }
 
     //hi
-    else if(command === 'hi'){
-        message.channel.send('hello'); 
+    else if(command === "hi"){
+        message.channel.send("hello"); 
     }
-
+ 
     //kiss 
-    else if (command == 'kiss'){
+    else if (command == "kiss"){
         if (!message.mentions.users.size) {
-            return message.reply('you need to tag someone first!');
+            return message.reply("you need to tag someone first!");
         }
         message.channel.send(`<:twodudes1:765817861175902208>${taggedUser}<:twodudes2:765817874652594186>`);
     } 
 
     //warning
-    else if(command == 'warn'){
+    //TODO: only I can warn people
+    //TODO: add counter for warns per person
+    else if(command == "warn"){
         if (!message.mentions.users.size) {
-            return message.reply('who???');
+            return message.reply("who???");
         }
-        message.channel.send(`:rage:  ${taggedUser}, you have been :warning:WARNED:warning:!  :rage:`)
+        message.channel.send(`:rage:  ${taggedUser}, you have been WARNED!  :rage:`)
     }
     
     //TODO
+    //add better text formatting
     //Word counter??
+    //add counter for warnings per user
+    //dm users like sending warnings
     //music bot??
     //valorant stats??
     //dice roll
@@ -101,7 +104,7 @@ client.on('message', message =>{
 
     //error message
     else{
-        message.channel.send('???');
+        message.channel.send("???");
     }
 });
 
