eadca4951024eaf517c98b7149fa6b6720474d5c
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2021-07-07 03:46:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const owner = message.client.users.cache.get('211241232737894400');
 
 // Prefix to call the bot
-const prefix="!"
+const prefix="!";
 
 // Logs once bot is back online
 client.once("ready", () => {
-    console.log("kevin bot is online!")
-    //message.send:211241232737894400("hi")
-})
+    console.log("kevin bot is online!");
+    message.send(`${owner} hi`);
+});
 
 
-client.on("message", message =>{
+client.on("message", message => {
     //to be fixed
-    if(!message.content == "pog"){
-        message.channel.send("poggers")
+    if (!message.content == "pog"){
+        message.channel.send("poggers");
     }
-    // ignore if no prefix
-    if(!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
+    // ignores all non-commands
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
     // grab the "first" mentioned user from the message
     // this will return a "user" object, just like "message.author"
-    const taggedUser = message.mentions.users.first()
+    const taggedUser = message.mentions.users.first();
 
     // Command Functions------------------------
     function commands(){
         // Gives a list of all the commands
-        console.log("!commands used")
+        console.log("!commands used");
         message.channel.send("```" +
         "!info to learn more about this bot\n" +
         "!update for the latest updates\n" +
@@ -41,11 +43,11 @@ client.on("message", message =>{
         "!coin to flip a coin\n" +
         "!cool-meter to see how cool you are (you can @ someone)\n" +
         "```"
-        )
+        );
     }
     function info(){
         // Prints info about bot
-        console.log("!info used")
+        console.log("!info used");
         message.channel.send(
             "```" +
             "Work in progress | Created in 10/13/2020\n"+
@@ -59,76 +61,77 @@ client.on("message", message =>{
     }
     function update(){
         // Prints update log
-        console.log("!update used")
+        console.log("!update used");
         message.channel.send("```" +
-            "Last Update: 7/06/21\n" +
+            "Last Update: 7/07/21\n" +
             "Bot will not respond to unknown commands\n" +
+            "The bot will be mean if you get 30% or less on cool-meter" +
             "```"
         );
     }
     function hi(){
-        console.log("!hi used")
-        message.channel.send("hello")
+        console.log("!hi used");
+        message.channel.send("hello");
     }
     function kiss(){
-        console.log("!kiss used")
+        console.log("!kiss used");
         if (!message.mentions.users.size) {
-            return message.reply("you need to tag someone first!")
+            return message.reply("you need to tag someone first!");
         }
-        message.channel.send(`<:twodudes1:768269580862095390>${taggedUser}<:twodudes2:765817874652594186>`)
+        message.channel.send(`<:twodudes1:768269580862095390>${taggedUser}<:twodudes2:765817874652594186>`);
 
     }
     function warn(){
         // TODO: add counter for warns per person
-        console.log("!warm used")
+        console.log("!warm used");
 
         if (!message.mentions.users.size) {
-            return message.reply("who???")
+            return message.reply("who???");
         }
-        message.channel.send(`:rage:  ${taggedUser}, you have been WARNED!  :rage:`)
+        message.channel.send(`:rage:  ${taggedUser}, you have been WARNED!  :rage:`);
 
     }
     function die(){
         // Rolls a 6 sided die!
         // Rolls a random number from 1-6
-        console.log("!die used")
-        var ranDie = Math.floor(Math.random() * Math.floor(6)) + 1
-        message.channel.send("You rolled a " + ranDie  + "!")
+        console.log("!die used");
+        var ranDie = Math.floor(Math.random() * Math.floor(6)) + 1;
+        message.channel.send("You rolled a " + ranDie  + "!");
     }
     function coin(){
         // A coin flips heads or tails
         // 1% chance to land on its side
         console.log("!coin used")
         if (Math.random() < 0.01) {
-            message.channel.send("Woah! The coin landed on its side! My wife left me!")
+            message.channel.send("Woah! The coin landed on its side! My wife left me!");
         }
         else if (Math.random() > 0.5){
-            message.channel.send("The coin landed on heads!")
+            message.channel.send("The coin landed on heads!");
         }
         else {
-            message.channel.send("The coin landed on tails!")
+            message.channel.send("The coin landed on tails!");
         }
     }
     function cool(){
         // Randomly generate the cool percentage for the user
         // A user can @ someone to see their cool-o-meter
         // TODO: each user is stuck with their cool percentage
-        console.log("!cool-meter used")
-        var coolPercentage = Math.floor(Math.random() * 100)
+        console.log("!cool-meter used");
+        var coolPercentage = Math.floor(Math.random() * 100);
         if (!message.mentions.users.size) {
             if (coolPercentage <= 30) {
-                return message.reply("you are " + coolPercentage + "% cool. L<:OMEGALUL:862175661211516929>OSER")
+                return message.reply("you are " + coolPercentage + "% cool. L<:OMEGALUL:862175661211516929>SER");
             }
             else {
-                return message.reply("you are " + coolPercentage + "% cool!")
+                return message.reply("you are " + coolPercentage + "% cool!");
             }
         }
         if (message.mentions.users.size){
             if (coolPercentage <= 30) {
-                return message.channel.send(`${taggedUser} is ` + coolPercentage + '% cool! L<:OMEGALUL:862175661211516929>OSER')
+                return message.channel.send(`${taggedUser} is ` + coolPercentage + '% cool! L<:OMEGALUL:862175661211516929>SER');
             }
             else {
-                return message.channel.send(`${taggedUser} is ` + coolPercentage + "% cool!")
+                return message.channel.send(`${taggedUser} is ` + coolPercentage + "% cool!");
             }
         }
     }
@@ -136,8 +139,8 @@ client.on("message", message =>{
     // !help
     if (command == "help"){
         // this command may be redundant
-        console.log("!help used")
-        commands()
+        console.log("!help used");
+        commands();
     }
     // !commands
     else if (command == "commands"){commands()}
@@ -174,4 +177,4 @@ client.on("message", message =>{
 })
 
 
-client.login(process.env.token)
+client.login(process.env.token);
