03db0effbf2b1a6274e541c5006c86f07fd06e68
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2021-07-07 04:00:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,17 +6,15 @@ const client = new Discord.Client();
 const prefix="!";
 
 // Logs once bot is back online
-client.once("ready", () => {
+client.once("ready", message, () => {
     console.log("kevin bot is online!");
-    
+    const owner = message.client.users.cache.get("211241232737894400");
+    owner.send("hi2");
 });
 
 
 client.on("message", message => {
     //to be fixed
-    //message.send(`${owner} hi`);
-    const owner = message.client.users.cache.get("211241232737894400");
-    //owner.send("hi2");
     if (!message.content == "pog"){
         message.channel.send("poggers");
     }
